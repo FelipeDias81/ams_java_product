c7afc109f8dc40d901964663fd727836d6e69f35
FelipeDias81 <felipedias6725@gmail.com>
2023-08-29 15:53:33 -0300

    ValidationErrors

--- src/main/java/com/example/productbackend/dtos/ProductRequest.java
+++ src/main/java/com/example/productbackend/dtos/ProductRequest.java
@@ -1,7 +1,13 @@
 package com.example.productbackend.dtos;
 
-public record ProductRequest(
-        String name,
-        double price) {
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 
+public record ProductRequest(
+        
+        @NotBlank(message = "Nome não pode ser em branco")
+        String name,
+
+        @Min(value = 0, message = "O valor mínimo para o preço é zero")
+        double price) {
 }
--- src/main/java/com/example/productbackend/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/example/productbackend/resources/exceptions/ResourceExceptionHandler.java
@@ -1,9 +1,11 @@
 package com.example.productbackend.resources.exceptions;
 
+import java.lang.reflect.Method;
 import java.time.Instant;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -16,8 +18,7 @@ public class ResourceExceptionHandler {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<StandardError> entityNotFoundEXception(
             EntityNotFoundException e,
-            HttpServletRequest request) 
-    {
+            HttpServletRequest request) {
         StandardError error = new StandardError();
         HttpStatus status = HttpStatus.NOT_FOUND;
 
@@ -30,4 +31,23 @@ public class ResourceExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> validationErrorsException(
+            MethodArgumentNotValidException e,
+            HttpServletRequest request) {
+        ValidationErrors error = new ValidationErrors();
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+
+        error.setTimeStamp(Instant.now());
+        error.setStatus(status.value());
+        error.setError("Validaton Errors");
+        error.setMessage(e.getMessage());
+        error.setPath(request.getRequestURI());
+
+        e.getBindingResult()
+         .getFieldErrors()
+         .forEach(err -> error.addError(err.getDefaultMessage()));
+
+        return ResponseEntity.status(status).body(error);
+    }
 }
